1a8366e75b6c9d22797626470ffb56d86d058718
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-06-09 05:18:38 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## PayPal Rest Api Payment Gateway
+
+Implements PayPal Rest Api as payment gateway
+
+#### License
+
+MIT
---
+++ paypalrestapi/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "PayPal Rest Api Payment Gateway",
+			"type": "module",
+			"label": _("PayPal Rest Api Payment Gateway")
+		}
+	]
