63f3c12cdcf033e4db28b102a1ae5a3d3012db10
Panayiotis Halouvas <phalouvas@gmail.com>
2024-02-27 15:38:46 +0000

    Update PayPal Standard Payments version and add invoice fees hook

--- paypalstandardpayments/paypal_standard_payments_gateway/hooks/sales_invoice.py
+++ paypalstandardpayments/paypal_standard_payments_gateway/hooks/sales_invoice.py
@@ -5,27 +5,30 @@ def add_invoice_fees(doc, method=None):
     if doc.doctype == "Sales Invoice":
         settings = frappe.get_single("PayPal Standard Payments Settings")
         if settings.account_fees:
-            request_data = frappe.request.get_data()
-            data = json.loads(request_data)
-            orderID = data['orderID']
-            integration_request = frappe.get_doc('Integration Request', orderID)
-            order = json.loads(integration_request.output)
-            fees = frappe.utils.flt(order["purchase_units"][0]["payments"]["captures"][0]["seller_receivable_breakdown"]["paypal_fee"]["value"])
-            if fees:
-                doc.append("taxes", {
-                    "charge_type": "Actual",
-                    "account_head": settings.paypal_account,
-                    "description": "PayPal Fees",
-                    "tax_amount": fees,
-                    "tax_rate": 0,
-                    "cost_center": settings.cost_center
-                })
-                doc.append("taxes", {
-                    "charge_type": "Actual",
-                    "account_head": settings.account_fees,
-                    "description": "PayPal Fees",
-                    "tax_amount": -fees,
-                    "tax_rate": 0,
-                    "cost_center": settings.cost_center
-                })
+            try:
+                request_data = frappe.request.get_data()
+                data = json.loads(request_data)
+                orderID = data['orderID']
+                integration_request = frappe.get_doc('Integration Request', orderID)
+                order = json.loads(integration_request.output)
+                fees = frappe.utils.flt(order["purchase_units"][0]["payments"]["captures"][0]["seller_receivable_breakdown"]["paypal_fee"]["value"])
+                if fees:
+                    doc.append("taxes", {
+                        "charge_type": "Actual",
+                        "account_head": settings.paypal_account,
+                        "description": "PayPal Fees",
+                        "tax_amount": fees,
+                        "tax_rate": 0,
+                        "cost_center": settings.cost_center
+                    })
+                    doc.append("taxes", {
+                        "charge_type": "Actual",
+                        "account_head": settings.account_fees,
+                        "description": "PayPal Fees",
+                        "tax_amount": -fees,
+                        "tax_rate": 0,
+                        "cost_center": settings.cost_center
+                    })
+            except Exception as e:
+                frappe.log_error(f"Error adding invoice fees: {str(e)}", "Sales Invoice")
             pass
